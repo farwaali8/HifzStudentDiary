f9f9245c135ab0456c3f2978642b72b0cb986505
farwaali8 <farwaali782@gmaail.com>
2023-06-23 05:50:59 +0500

    Third Commit

--- app/src/main/java/com/example/madrasataskmanager/TaskAssign.java
+++ app/src/main/java/com/example/madrasataskmanager/TaskAssign.java
@@ -2,15 +2,28 @@ package com.example.madrasataskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import com.example.madrasataskmanager.Student;
 
 public class TaskAssign extends AppCompatActivity {
+    //Button nextButton = findViewById(R.id.button56);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_assign);
+        Button nextButton = findViewById(R.id.button56);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform the action when the button is clicked
+                Intent intent = new Intent(TaskAssign.this, TaskDetails.class);
+                startActivity(intent);
+            }
+        });
+
 
         // Retrieve the Intent that started this activity
         Intent intent = getIntent();
